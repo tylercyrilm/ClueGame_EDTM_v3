e0b13c115a0df1b8e03408da79d9b148424c8282
Becca May <remay@mines.edu>
2016-10-04 15:21:49 -0600

    Added loadConfigFileTests.java

---
+++ src/tests/loadConfigFileTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import static org.junit.Assert.*;
+import java.util.Set;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import experiment.BoardCell;
+import experiment.IntBoard;
+
+public class loadConfigFileTests{
+	@BeforeClass
+
+
+	//Test to ensure legend file is loaded correctly
+		//correct num of rooms, test several entries including first and last in file
+
+
+
+	//Test to ensure the correct number of rows/columns have been read
+
+
+
+	//Test to verifty at least one doorway in each direction.
+	//Also verify cells that don't contain doorways return false for isDoorway
+
+
+	//Test that the correct number of doors have been loaded
+
+
+
+	//Test some of the cells to ensure they have the correct initial
+	
+	
+	
+	
+}
