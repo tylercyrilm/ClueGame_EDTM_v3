b04770206c2ddebf4f7153be7fabaa9eaaeda128
Erica Dettmer-Radtke <edettmer@mymail.mines.edu>
2016-11-04 20:24:28 -0600

    Added skeleton code to DetectiveNotes.

--- src/clueGame/DetectiveNotes.java
+++ src/clueGame/DetectiveNotes.java
@@ -1,15 +1,83 @@
 package clueGame;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
 
 public class DetectiveNotes extends JDialog{
 
+	private JTextField guess, result, roll, whoseTurn;
+	
 	public DetectiveNotes() {
 		setTitle("Detective Notes");
-		setSize(300,100);
+		setSize(400,600);
+		
+		setLayout(new GridLayout(3,2));
+		JPanel panel = createPeople();
+		add(panel);
+		panel = createPersonGuess();
+		add(panel);
+		panel = createRooms();
+		add(panel);
+		panel = createRoomGuess();
+		add(panel);
+		panel = createWeapon();
+		add(panel);
+		panel = createWeaponGuess();
+		add(panel);
 		
 	}
+
+	private JPanel createPeople() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "People"));
+		return panel;
+	}
+	
+	private JPanel createPersonGuess() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Person Guess"));
+		return panel;
+	}
+	
+
+	private JPanel createRooms() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Rooms"));
+		return panel;
+	}
+	
+	 private JPanel createRoomGuess() {
+		 	JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder (new EtchedBorder(), "Room Guess"));
+			return panel;
+	}
+	 
+	 private JPanel createWeapon() {
+		 	JPanel panel = new JPanel();
+			panel.setBorder(new TitledBorder (new EtchedBorder(), "Weapons"));
+			return panel;
+	}
+
+	 
+	private JPanel createWeaponGuess() {
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder (new EtchedBorder(), "Weapon Guess"));
+		return panel;
+	}
+
+
+
+
+	
+	
 	
 	
 }
